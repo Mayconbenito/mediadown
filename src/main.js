bafd1e51b0aee544db3fb56980b3b27ca7052f66
Maycon Benito <mayki17321@gmail.com>
2020-06-04 11:35:21 -0300

    feature: show download status, progress and media type

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+require("update-electron-app")({
+  repo: "mayconbenito/mediadown",
+  updateInterval: "1 hour",
+});
 const { app, BrowserWindow, ipcMain, shell } = require("electron");
 const ytdl = require("ytdl-core");
 const path = require("path");
@@ -16,26 +20,25 @@ function createWindow() {
     maxWidth: 400,
     maxHeight: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
     autoHideMenuBar: true,
-    icon: path.join(__dirname, "../", "build/icon-x32.png")
+    icon: path.join(__dirname, "../", "build/icon-x32.png"),
   });
 
   mainWindow.loadFile(path.join(__dirname, "public", "index.html"));
-
-  mainWindow.on("closed", function() {
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
 }
 
 app.on("ready", createWindow);
 
-app.on("window-all-closed", function() {
+app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
 
-app.on("activate", function() {
+app.on("activate", function () {
   if (mainWindow === null) createWindow();
 });
 
@@ -43,59 +46,68 @@ const filePath = path.join(app.getPath("documents"), "MediaDown - Downloads");
 
 const downloadList = [];
 
-ipcMain.on("downloadFromURL", async function(event, response) {
-  if (!ytdl.validateURL(response.url)) {
-    mainWindow.webContents.send("invalidURL", {});
-    return;
-  }
+ipcMain.on("downloadFromURL", async function (event, response) {
+  try {
+    if (!ytdl.validateURL(response.url)) {
+      mainWindow.webContents.send("invalidURL", {});
+      return;
+    }
 
-  if (!fs.existsSync(filePath)) {
-    await promisify(fs.mkdir)(filePath);
-  }
+    if (!fs.existsSync(filePath)) {
+      await promisify(fs.mkdir)(filePath);
+    }
 
-  let filter;
-  let fileExtension;
-  if (response.option === "audioonly") {
-    filter = "audioonly";
-    fileExtension = ".mp3";
-  }
+    let filter;
+    let fileExtension;
+    if (response.option === "audioonly") {
+      filter = "audioonly";
+      fileExtension = ".mp3";
+    }
 
-  if (response.option === "normal") {
-    filter = "video";
-    fileExtension = ".mp4";
-  }
+    if (response.option === "normal") {
+      filter = "video";
+      fileExtension = ".mp4";
+    }
 
-  const info = await promisify(ytdl.getInfo)(response.url);
+    const info = await ytdl.getInfo(response.url);
 
-  downloadList.push({
-    url: response.url,
-    progress: 0
-  });
-
-  const stream = ytdl(response.url, { filter });
-
-  stream.pipe(
-    fs.createWriteStream(path.join(filePath, info.title + fileExtension))
-  );
-
-  stream.on("progress", function(chunkLength, downloaded, total) {
-    const percent = downloaded / total;
-
-    const index = downloadList.findIndex(item => item.url === response.url);
-    downloadList[index].progress = (percent * 100).toFixed(2);
-
-    mainWindow.webContents.send("download", {
-      id: info.video_id,
-      title: info.title,
-      thumbnail:
-        info.player_response.videoDetails.thumbnail.thumbnails[2].url || "",
-      progress: percent,
-      filter
+    downloadList.push({
+      url: response.url,
+      progress: 0,
     });
-  });
+
+    const stream = ytdl(response.url, { filter });
+
+    stream.pipe(
+      fs.createWriteStream(
+        path.join(filePath, info.videoDetails.title + fileExtension)
+      )
+    );
+
+    stream.on("progress", function (chunkLength, downloaded, total) {
+      const percent = downloaded / total;
+
+      const index = downloadList.findIndex((item) => item.url === response.url);
+      downloadList[index].progress = (percent * 100).toFixed(2);
+
+      mainWindow.webContents.send("download", {
+        id: info.videoId,
+        title: info.videoDetails.title,
+        thumbnail:
+          info.playerResponse.videoDetails.thumbnail.thumbnails[2].url || "",
+        progress: percent,
+        filter,
+      });
+    });
+  } catch (err) {
+    mainWindow.webContents.send("error", {
+      error: true,
+    });
+    console.log(err);
+  }
 });
 
-ipcMain.on("openDownloadFolder", async function(event, response) {
+ipcMain.on("openDownloadFolder", async function (event, response) {
   if (!fs.existsSync(filePath)) {
     await promisify(fs.mkdir)(filePath);
   }
